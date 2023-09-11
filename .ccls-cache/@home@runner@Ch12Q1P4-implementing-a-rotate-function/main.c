a87bd7e2860b961286de76b5c5ba456ce2784f17
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-11 13:30:10 +0000

    copied program

--- .ccls-cache/@home@runner@Ch12Q1P4-implementing-a-rotate-function/main.c
+++ .ccls-cache/@home@runner@Ch12Q1P4-implementing-a-rotate-function/main.c
@@ -1,6 +1,46 @@
+// Program to illustrate the rotation of integers
+
 #include <stdio.h>
 
+// Declare Functions
+unsigned int rotate (unsigned int, int);
+
 int main(void) {
-  printf("Hello World\n");
+
+  unsigned int w1 = 0xabcdef00u, w2 = 0xffff1122u;
+
+  printf ("  Rotate     BY     Equals\n");
+  printf ("--------   ----   --------\n");
+  printf ("%.6x   %.4d   %.6x\n", w1, 8, rotate (w1, 8));
+  printf ("%.6x   %.4d   %.6x\n", w1, -16, rotate (w1, -16));
+  printf ("%.6x   %.4d   %.6x\n", w2, 4, rotate (w2, 4));
+  printf ("%.6x   %.4d   %.6x\n", w2, -2, rotate (w2, -2));
+  printf ("%.6x   %.4d   %.6x\n", w1, 0, rotate (w1, 0));
+  printf ("%.6x   %.4d   %.6x\n", w1, 44, rotate (w1, 44));
+  
   return 0;
+}
+
+// Define Functions
+unsigned int rotate (unsigned int value, int n) {
+  unsigned int result, bits;
+
+  if (n > 0)
+    n %= 32;
+  else
+    n = -(-n % 32);
+
+  if (n == 0)
+    result = value;
+  else if (n > 0) { // Rotate left
+    bits = value >> (32 - n);
+    result = value << n | bits;
+  }
+  else { // Rotate right
+    n = -n;
+    bits = value << (32 - n);
+    result = value >> n | bits;
+  }
+
+  return result;
 }
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Programming in C
+## Chapter 12
+### Question 1: Part 4
+
+This problem is from chapter 12 of the textbook[^1]. This program[^2] rotates a binary number.
+
+You may wish to run the program and compare its output.
+
+
+[^1]: Programming in C (Third edition) By Stephen G. Kochan - Page 297
+[^2]: Programming in C (Third edition) By Stephen G. Kochan - Page 290 : program 12.4
